5edc1ea870e3816e3a2674b969738bcda8bea98d
Trevor Ross <trevor.is.ross@gmail.com>
2022-04-09 23:00:28 -0400

    added instructions for installing app

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -25,6 +25,47 @@
         </v-list-item-content>
       </v-list-item>
     </v-list>
+
+    <br />
+    <v-list class="install-app">
+      <v-list-group
+        :value="false"
+        no-action
+        prepend-icon="mdi-download"
+        color="white"
+      >
+        <template v-slot:activator>
+          <v-list-item-content>
+            <v-list-item-title>How To Install App</v-list-item-title>
+          </v-list-item-content>
+        </template>
+
+        <v-list-item class="step">
+          <v-list-item-icon class="step-icon">
+            <v-icon large> mdi-numeric-1 </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <img src="../assets/install_1.png" />
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item class="step">
+          <v-list-item-icon class="step-icon">
+            <v-icon large> mdi-numeric-2 </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <img src="../assets/install_2.png" />
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item class="step">
+          <v-list-item-icon class="step-icon">
+            <v-icon large> mdi-numeric-3 </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <img src="../assets/install_3.png" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-group>
+    </v-list>
   </div>
 </template>
 
@@ -43,6 +84,7 @@ export default {
       { title: "Settings", icon: "mdi-cog", path: "/settings" },
     ],
     right: null,
+    expandInstallApp: false,
   }),
   computed: {
     ...mapGetters([
@@ -61,5 +103,18 @@ export default {
 
 h1 {
   margin-bottom: 2rem;
+}
+
+.install-app img {
+  width: 100%;
+  border-radius: 5px;
+  max-width: 20rem;
+}
+
+.install-app .step {
+  padding-left: 1rem !important;
+}
+.step-icon {
+  margin: auto;
 }
 </style>
